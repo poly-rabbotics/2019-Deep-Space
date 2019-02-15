4a6aac979c6fe0e8d355610dba484a6a5c7f4dd3
MelissaGWhite <35669711+MelissaGWhite@users.noreply.github.com>
2019-02-14 18:04:30 -0800

    Added a bracket

--- src/main/java/frc/robot/commands/LiftSystemCommand.java
+++ src/main/java/frc/robot/commands/LiftSystemCommand.java
@@ -8,6 +8,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 
 public class LiftSystemCommand extends Command {
   public LiftSystemCommand() {
@@ -24,6 +25,7 @@ public class LiftSystemCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+  }
   
 
   // Make this return true when this Command no longer needs to run execute()
