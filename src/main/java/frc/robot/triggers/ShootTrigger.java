ac53bea4409e12ffa8ad698efd9a8140f1b0a004
petervdonovan <33707478+petervdonovan@users.noreply.github.com>
2019-02-17 11:38:06 -0800

    Update ShootTrigger.java

--- src/main/java/frc/robot/triggers/ShootTrigger.java
+++ src/main/java/frc/robot/triggers/ShootTrigger.java
@@ -10,6 +10,6 @@ import edu.wpi.first.wpilibj.buttons.Trigger;
 public class ShootTrigger extends Trigger {
 
     public boolean get() {
-        return Robot.controlChooser.getSelected().getShoot();
+        return Robot.controlChooser.getSelected().getToggleOutwards();
     }
 }
