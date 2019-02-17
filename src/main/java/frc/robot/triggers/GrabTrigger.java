db433209bad4ddaeb6f1434858bb632e6118baf3
petervdonovan <33707478+petervdonovan@users.noreply.github.com>
2019-02-17 11:37:47 -0800

    Update GrabTrigger.java

--- src/main/java/frc/robot/triggers/GrabTrigger.java
+++ src/main/java/frc/robot/triggers/GrabTrigger.java
@@ -10,6 +10,6 @@ import edu.wpi.first.wpilibj.buttons.Trigger;
 public class GrabTrigger extends Trigger {
 
     public boolean get() {
-        return Robot.controlChooser.getSelected().getGrab();
+        return Robot.controlChooser.getSelected().getToggleInwards();
     }
 }
