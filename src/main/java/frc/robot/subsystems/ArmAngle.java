00703d454bf38db595e3fb13e8afdc4d51bf9d6c
petervdonovan <33707478+petervdonovan@users.noreply.github.com>
2019-03-11 22:19:59 -0700

    Rewrite ArmAngle.java with untested code

--- src/main/java/frc/robot/subsystems/ArmAngle.java
+++ src/main/java/frc/robot/subsystems/ArmAngle.java
@@ -11,37 +11,59 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.Talon;               //Are we just using Victor SPX ? Why are we not using PWMVictorSPX class?
 import edu.wpi.first.wpilibj.VictorSP;
 import frc.robot.RobotMap;
+import frc.robot.sensors.CTREMagneticEncoder;
+import edu.wpi.first.wpilibj.PIDController;
+
 /**
  * Add your docs here.
  */
+
 public class ArmAngle extends Subsystem {
+  
+  private static final double DOWN_DEGREES = 0;         //TODO: Add real value
+  private static final double ROCKET_DEGREES = 30;
+  private static final double CARGO_SHIP_DEGREES = 45;
+  private static final double UP_DEGREES = 75;
+  private static final double[] POSITIONS = {DOWN_DEGREES, ROCKET_DEGREES, CARGO_SHIP_DEGREES, UP_DEGREES};
+  
+  private int position;
+  
   private VictorSP angle = RobotMap.wheelArmAngle;
-  private static double armAngleSpeed = .5;//TODO: Add real value
-  public boolean moving = false;
+  private CTREMagneticEncoder armEncoder = RobotMap.wheelArmEncoder;
+  private PIDController armController = new PIDController(0.1, 0.01, 0.1, armEncoder, angle);
+  //private static double armAngleSpeed = .5;   //TODO: Add real value
+  //public boolean moving = false;
   public ArmAngle(){
     super("Arm Angle");
     addChild("Angle Motor", angle);
-  }
-  public void spinUpwards(){
-    angle.set(armAngleSpeed);
-    moving = true;
-  }
-  public void spinDownwards(){
-    angle.set(-armAngleSpeed);
-    moving = true;
-  }
-  public void stopSpin(){
-    angle.set(0);
-    moving = false;
-  }
-  public boolean getMoving(){
-    return moving;
+    position = 3;
+    armController.enable();
   }
   
+  public void spinUpwards() {
+    if (position < 3)
+      position++;
+    
+    armController.setSetpoint(POSITIONS[position]);
+  }
+  public void spinDownwards() {
+    if (position > 0)
+      position--;
+    
+    armController.setSetpoint(POSITIONS[position]);
+  }
+   
   @Override
   public void initDefaultCommand() {
 
     // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+  
+    
+    // setDefaultCommand(new M     *   
+    
+    
+    
+  // * 
+   //*     
   }
 }
