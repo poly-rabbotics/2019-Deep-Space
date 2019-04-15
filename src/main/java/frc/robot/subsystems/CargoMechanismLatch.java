debb8780c151e855dfd0c96dce2936e05524f9c1
petervdonovan <33707478+petervdonovan@users.noreply.github.com>
2019-04-15 15:11:48 -0700

    Update CargoMechanismLatch.java

--- src/main/java/frc/robot/subsystems/CargoMechanismLatch.java
+++ src/main/java/frc/robot/subsystems/CargoMechanismLatch.java
@@ -5,24 +5,43 @@ import frc.robot.RobotMap;
 
 public class CargoMechanismLatch extends Subsystem {
   private Talon latchMotor;
-  public static final double CURRENT_THRESHOLD = 9.0;  //stall current is 12A
+  private static final double CURRENT_THRESHOLD = 7.0;  //stall current is 12A
+  private static final double STEP = 0.1;
+  private static final double TOLERANCE = 0.3;
+  private static final double DOWN_DELAY = 1.2; //seat motor is 24 rpm if no mechanical resistance. This should be time for almost 180 degree rotation
+  
+  private static Timer t.start();
+  
+  private double motorControl;
+  private double t_up_last;
+  
   public CargoMechanismLatch(){
     super("CargoMechanismLatch");
     latchMotor = RobotMap.latchMotor;
+    motorControl = 0;
+    timeLast = 0;                        //If the mechanism doesn't start the game rotated all the way up, there will be problems. it will rotate down too much
     addChild("latch motor", latchMotor);
   }
   /*
   * Postcondition: return false if unable to continue moving upwards
   */
-  public boolean spinUpwards() {
+  public void spinUpwards() {
     double current = pdp.getCurrent(1); //TODO: CHANGE TO ACTUAL PORT VALUE
-    if (current > CURRENT_THRESHOLD)
+    if (current > CURRENT_THRESHOLD + TOLERANCE)
     {
-      latchMotor.set(0.1);
-      return false;
+      motorControl -= STEP;     //TODO: Make sure this spins it the correct direction
+    }
+    else if (current < CURRENT_THRESHOLD - TOLERANCE) {
+      motorControl += STEP;
+    }
+    latchMotor.set(motorControl);
+    timeLast = t.get();
+  }
+  public void spinDownwards() {
+    if (t.get() - timeLast > 1.2) {
+      latchMotor.set(-1);
     }
     else {
-      latchMotor.set(1);
-      return true;
+      latchMotor.set(0);
     }
   }
